c3989314edbffb51ff86cbace8ec5874d827802c
Juan Cuzmar <jcuzmar@protonmail.com>
2021-12-16 19:34:53 -0300

    first commit

---
+++ packages/body/src/App.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="mt-10 text-3xl mx-auto max-w-6xl">
+    <div>Name: body</div>
+    <div>Framework: vue3</div>
+    <div>Language: JavaScript</div>
+    <div>CSS: Tailwind</div>
+  </div>
+</template>
